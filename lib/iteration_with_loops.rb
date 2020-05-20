550d741bb24b83316591a0d90e7cf7099451a6bb
eager-class-5814 <candacehchoe@gmail.com>
2020-05-20 00:35:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,6 @@ row_index = 0
 while row_index < src.count do
   element_index = 0 
   while element_index < src[row_index].count do
-  binding.pry
   if src[row_index][element_index] < min_number
       min_number = src[row_index][element_index]
   end
